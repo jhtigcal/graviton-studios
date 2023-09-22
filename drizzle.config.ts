2dda9ba01d86cff12f4003c955b0416448c2a830
jhtigcal <jhtigcal@gmail.com>
2023-09-22 23:32:30 +0800

    feat: implemented authentication

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,6 @@
 import 'dotenv/config';
 import { type Config } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set');
-}
-
 export default {
   schema: './lib/db/schema.ts',
   out: './drizzle',
--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -1,6 +1,34 @@
-// import type { NextAuthOptions } from 'next-auth';
-// import { DrizzleAdapter } from '@auth/drizzle-adapter'
+import { db } from '@/lib/db';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import type { NextAuthOptions } from 'next-auth';
 
-// export const authOptions: NextAuthOptions = {
-//   adapter: DrizzleAdapter
-// };
+import DiscordProvider from 'next-auth/providers/discord';
+import EmailProvider from 'next-auth/providers/email';
+
+// import { sendVerificationRequest } from './helpers/send-verification-request';
+
+export const authOptions: NextAuthOptions = {
+  adapter: DrizzleAdapter(db),
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.SMTP_SERVER,
+        port: Number(process.env.SMTP_PORT),
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+      // sendVerificationRequest,
+    }),
+  ],
+};
